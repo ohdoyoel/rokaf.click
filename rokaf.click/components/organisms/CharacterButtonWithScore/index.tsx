115dd8cd5e7d45c15239ced4ba5951458a2ed33d
Daniel Oh <ohdoyoel@naver.com>
2023-10-03 11:31:34 +0000

    [feat] Score ++ function added

--- rokaf.click/components/organisms/CharacterButtonWithScore/index.tsx
+++ rokaf.click/components/organisms/CharacterButtonWithScore/index.tsx
@@ -1,19 +1,28 @@
+'use client'
+
+import { useState } from 'react'
 import { CharacterButton } from "@/components/molecules/CharacterButton";
 import { Score } from "@/components/atoms/Score";
 
 interface CharacterButtonWithScoreProps {
     imageSource: string;
     size?: number;
-    onClick?: () => void;
 }
 
 // score update code needed
 
-export const CharacterButtonWithScore = ({imageSource, size, onClick}: CharacterButtonWithScoreProps) => {
+export const CharacterButtonWithScore = ({imageSource, size}: CharacterButtonWithScoreProps) => {
+    const [score, setScore] = useState(0)
+    const characterButtonClick = () => {
+        setScore(score + 1)
+        // location's score + 1 on ranking bar
+        // location's score + 1 on json server
+    }
+
     return (
-        <div>
-        <Score score={777}/>
-        <CharacterButton imageSource={imageSource} size={size} onClick={onClick}/>
+        <div className='transition ease-in-out duration-150 hover:scale-110 active:scale-105'>
+        <Score score={score}/>
+        <CharacterButton imageSource={imageSource} size={size} onClick={characterButtonClick}/>
         </div>
     )
 }
