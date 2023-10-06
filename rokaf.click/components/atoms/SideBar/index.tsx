4b6f599e42aa9142ca5b0dd70378443ac80b0c4e
Daniel Oh <ohdoyoel@naver.com>
2023-10-06 14:02:27 +0000

    [design] location bar responsive design

--- rokaf.click/components/atoms/SideBar/index.tsx
+++ rokaf.click/components/atoms/SideBar/index.tsx
@@ -6,9 +6,8 @@ interface SideBarProps {
 
 export const SideBar = ({children}: SideBarProps) => {
     return (
-        <div className='w-20 h-full p-3
-                        bg-slate-300 shadow
-                        transition-all ease-in-out duration-150 hover:w-80'>
+        <div className='w-full h-full p-3
+                        bg-slate-300 shadow'>
             {children}
         </div>
     )
--- rokaf.click/components/organisms/LocationBar/index.tsx
+++ rokaf.click/components/organisms/LocationBar/index.tsx
@@ -2,10 +2,20 @@ import { SideBar } from "@/components/atoms/SideBar"
 import { SearchLocation } from "@/components/molecules/SearchLocation"
 
 export const LocationBar = () => {
-    return ( // responsive design needed
-        <div className='absolute inset-y-52 left-0 rounded-r-lg'>
+    return (
+        <div className='absolute inset-y-52 left-0 rounded-r-lg
+                        w-20 transition-all ease-in-out duration-150 hover:w-80
+                        group'>
             <SideBar>
-                <SearchLocation/>
+                <div className="hidden group-hover:block">
+                    <SearchLocation/>
+                </div>
+                <div className="group-hover:hidden
+                                grid absolute inset-0 place-content-center">
+                    <p className="font-slab-bold">
+                        부대 바꾸기
+                    </p>
+                </div>
             </SideBar>
         </div>
     )
