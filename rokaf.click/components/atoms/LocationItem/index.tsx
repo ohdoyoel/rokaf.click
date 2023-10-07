c2b1d2d0b08adbac3471d4171db649ce2a006443
Daniel Oh <ohdoyoel@naver.com>
2023-10-07 15:40:02 +0000

    [design] logo files

--- rokaf.click/components/atoms/LocationItem/index.tsx
+++ rokaf.click/components/atoms/LocationItem/index.tsx
@@ -1,15 +1,24 @@
+import { LogoImage } from "../LogoImage";
+
 interface LocationItemProps {
     id: number; // receive all data from parent looks better
 }
 
 export const LocationItem = ({id}:LocationItemProps) => {
+    const name = "공군본부" // name from id
+    const logoId = 1 // logo id from id
+
     return (
         <li>
-            <div className="flex w-full items-center pl-2 rounded hover:bg-gray-100">
+            <div className="flex gap w-9/12 items-center pl-2 rounded hover:bg-gray-100
+                            transition-all ease-in-out duration-150 hover:scale-105">
                 <input type="radio" name="loc" value={id}
                     className="w-4 h-4 bg-gray-100 checked:bg-gray-100 border-gray-300 rounded"/>
+                <div className="pl-3">
+                    <LogoImage logoId={logoId} size={30}/>
+                </div>
                 <label className="w-full py-2 ml-2 text-sm font-medium text-gray-900 rounded">
-                    3여단 188대대 5포대
+                    {name}
                 </label>
             </div>
         </li>
