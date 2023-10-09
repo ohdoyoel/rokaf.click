a58bee7d50e02c61cbad9c3afd6488270eb1aaf4
Daniel Oh <ohdoyoel@gmail.com>
2023-10-09 05:57:24 +0000

    [feat] location bar with axios

--- rokaf.click/src/components/organisms/CharacterButtonWithScore/index.tsx
+++ rokaf.click/src/components/organisms/CharacterButtonWithScore/index.tsx
@@ -1,4 +1,4 @@
-'use client'
+// 'use client'
 
 import { useEffect, useState } from 'react'
 import { CharacterButton } from "@/src/components/molecules/CharacterButton";
--- rokaf.click/src/components/organisms/LocationBar/index.tsx
+++ rokaf.click/src/components/organisms/LocationBar/index.tsx
@@ -1,7 +1,9 @@
+import { getLocations } from "@/src/api/location"
 import { SideBar } from "@/src/components/atoms/SideBar"
 import { SearchLocation } from "@/src/components/molecules/SearchLocation"
 import { Api } from "@/src/types/data"
 import { Location } from "@/src/types/data"
+import axios from "axios"
 import { Dispatch, SetStateAction, useEffect, useState } from "react"
 
 const context: Api = {
@@ -14,18 +16,42 @@ interface LocationBarProps {
 
 export const LocationBar = ({setLocationId}: LocationBarProps) => {
     const [locations, setLocations] = useState<Location[]>([])
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
     
-    useEffect(() => {
-        fetch(context.apiRootUrl + "/locations",
-                {
-                    method:'GET',
-                    headers: {'Content-Type':'application/json'},
-                })
-            .then((res) => res.json())
-            .then((data) => setLocations(data));
-    }, [])
+    // useEffect(() => {
+    //     fetch(context.apiRootUrl + "/locations",
+    //             {
+    //                 method:'GET',
+    //                 headers: {'Content-Type':'application/json'},
+    //             })
+    //         .then((res) => res.json())
+    //         .then((data) => setLocations(data))
+    //         .catch((error) => (window.alert(error)))
+    // }, [])
 
-    // console.log(locations)
+    useEffect(() => {
+        const fetchLocations = async () => {
+            try {
+                // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+                setError(false);
+                // loading 상태를 true 로 바꿉니다.
+                setLoading(true);
+                const response = await axios.get(
+                    context.apiRootUrl + "/locations",
+                );
+                setLocations(response.data); // 데이터는 response.data 안에 들어있습니다.
+            } catch (e) {
+                setError(true)
+            }
+            setLoading(false)
+        };
+
+        fetchLocations();
+    }, []);
+
+    // if (loading) return <div>로딩중..</div>;
+    // if (error) return <div>에러가 발생했습니다</div>;
 
     return (
         <div className='absolute inset-y-52 left-0 rounded-r-lg
@@ -34,6 +60,8 @@ export const LocationBar = ({setLocationId}: LocationBarProps) => {
             <SideBar>
                 <div className="hidden group-hover:block">
                     <SearchLocation setLocationId={setLocationId} locations={locations}/>
+                    {loading ? <div>로딩중..</div> : null}
+                    {error ? <div>에러가 발생했습니다</div> : null}
                 </div>
                 <div className="group-hover:hidden
                                 grid absolute inset-0 place-content-center">
