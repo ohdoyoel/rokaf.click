b4a5d2fefbc504f57cebeb1b213f66df13f07483
Daniel Oh <ohdoyoel@gmail.com>
2023-10-14 20:29:52 +0900

    [design] responsive design for image

--- rokaf.click/src/components/atoms/CharacterImage/index.tsx
+++ rokaf.click/src/components/atoms/CharacterImage/index.tsx
@@ -9,10 +9,9 @@ export const CharacterImage = ({id, size}: CharacterImageProps) => {
     const imageSource = '/images/characters/' + ((id < 10) ? `0${id}` : `${id}`) + '.png' 
     return (
         <img // change this to Image when build next app
+        className="max-h-full w-auto"
         src={imageSource}
         alt={imageSource}
-        width={size}
-        height={size}
         />
     )
 }
