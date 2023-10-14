10335d0eb14c8f441c2ed0e3379372e1f24ddb43
Daniel Oh <ohdoyoel@gmail.com>
2023-10-14 19:51:48 +0900

    [delete] stories and memo

--- rokaf.click/src/components/atoms/Header/index.stories.tsx
+++
@@ -1,6 +0,0 @@
-import { Meta } from '@storybook/react'
-import { Header } from './index'
-
-export default { title: 'components/atoms/Header' } satisfies Meta<typeof Header>
-
-export const standard = () => <Header/>
--- rokaf.click/src/components/atoms/SideBar/index.stories.tsx
+++
@@ -1,6 +0,0 @@
-import { Meta } from '@storybook/react'
-import { SideBar } from './index'
-
-export default { title: 'components/atoms/CollapsedBar' } satisfies Meta<typeof SideBar>
-
-export const standard = () => <SideBar/>
