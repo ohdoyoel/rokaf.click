707814fbaea237a66b1581146d45f409800b89d9
Daniel Oh <ohdoyoel@gmail.com>
2023-10-14 19:47:35 +0900

    minor

--- rokaf.click/src/components/atoms/CharacterImage/index.stories.tsx
+++
@@ -1,6 +0,0 @@
-import { Meta } from '@storybook/react'
-import { CharacterImage } from './index'
-
-export default { title: 'components/atoms/CharacterImage' } satisfies Meta<typeof CharacterImage>
-
-export const character01 = () => <CharacterImage imageSource='/images/characters/01.png' size={400}/>
