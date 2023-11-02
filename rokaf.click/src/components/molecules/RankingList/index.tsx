fce42ce041635186a389b218e4f08982a2090259
Daniel Oh <ohdoyoel@gmail.com>
2023-11-02 15:55:55 +0000

    [fixed] ranking list re-render complicate 고침

--- rokaf.click/src/components/molecules/RankingList/index.tsx
+++ rokaf.click/src/components/molecules/RankingList/index.tsx
@@ -1,6 +1,6 @@
 import { RankingItem } from "@/src/components/atoms/RankingItem"
 import { Location } from "@/src/types/data"
-import { Dispatch, SetStateAction } from "react"
+import { Dispatch, SetStateAction, useEffect, useLayoutEffect, useRef, useState } from "react"
 
 interface RankingListProps {
     rank: number
@@ -10,25 +10,33 @@ interface RankingListProps {
 }
 
 export const RankingList = ({rank, setRank, locationId, sortedLocations}: RankingListProps ) => {
+    const [list, setList] = useState<React.JSX.Element[]>([])
 
     const listAllSortedLocations = () => {
+        console.log(`list build`)
         let rank = 1
         const result = []
         for (const location of sortedLocations) {
             result.push(<RankingItem key={location.id} rank={rank} id={location.id} logoSrc={location.logoSrc}
                                     name={location.name} score={location.score}
                         />)
-            if (location.id == locationId) { setRank(rank) }
+            if (location.id == locationId) {
+                setRank(rank)
+            }
             rank++
         }
         return result
     }
 
+    useEffect(() => {
+        setList(listAllSortedLocations())
+    }, [sortedLocations, locationId])
+
     return (
         <div className="flex flex-col items-start">
             <p className="text-xl text-center p-2">🏅 리더보드 {locationId} {rank}</p>
             <ul id="locationList" className="h-full w-full p-1 overflow-x-hidden overflow-y-auto text-sm text-gray-700">
-                {listAllSortedLocations()}
+                {list}
             </ul>
         </div>
     )
