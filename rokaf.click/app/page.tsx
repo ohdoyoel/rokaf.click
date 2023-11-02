5c18ade4439c6fe4e235ffb6fb09b717d1155de4
Daniel Oh <ohdoyoel@gmail.com>
2023-11-02 15:18:53 +0000

    [fix] id가 중복으로 넘어가는 문제 해결 [feat] id 바꿀 때 patch 추가

--- rokaf.click/app/page.tsx
+++ rokaf.click/app/page.tsx
@@ -1,16 +1,69 @@
 'use client'
 
-import { useRef, useState } from 'react'
+import { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { Header } from '@/src/components/atoms/Header'
 import { CharacterButtonWithScore } from '@/src/components/organisms/CharacterButtonWithScore'
 import { LocationBar } from '@/src/components/organisms/LocationBar'
 import { CharacterBar } from '@/src/components/organisms/CharacterBar'
 import { RankingBar } from '@/src/components/organisms/RankingBar'
+import axios from 'axios'
 
 export default function Home() {
   const [imageId, setImageId] = useState(1)
   const [locationId, setLocationId] = useState(0)
-  console.log(`locationId: ${locationId}`)
+  const locationIdRef = useRef(0)
+  const [score, setScore] = useState(0)
+  const scoreRef = useRef(0)
+  const locationScoreRef = useRef(-1000)
+
+  // get location score
+  const getLocationScore = async (_id: number) => {
+      let score: number = -999
+      try {
+          const response = await axios.get(
+              process.env.NEXT_PUBLIC_API_BASE_PATH + `locations/${_id}`,
+              )
+              score = response.data.score
+      } catch (e) {
+          console.log(e)
+      } finally {
+          locationScoreRef.current = score
+          console.log(`GET id:${_id} score:${locationScoreRef.current}`)
+      }
+  }
+      
+  // as location id change, call getLocationScore
+  useEffect(() => {
+    locationIdRef.current = locationId
+    locationIdRef.current != 0 && getLocationScore(locationIdRef.current)
+  }, [locationId])
+
+  // patch location score
+  const patchLocationScore = async (_id: number) => {
+      console.log(`PATCH id: ${_id} locationScore: ${locationScoreRef.current}, score: ${scoreRef.current}`)
+      try {
+          axios.patch(
+              process.env.NEXT_PUBLIC_API_BASE_PATH + `locations/${_id}`,
+              {"score": locationScoreRef.current + scoreRef.current}
+          )
+      } catch (e) {
+          console.log(e)
+      }
+  }
+
+  // as location id change, call patchLocationScore
+  useLayoutEffect(() => {
+      console.log(locationIdRef.current)
+      locationIdRef.current != 0 && patchLocationScore(locationIdRef.current)
+  }, [locationId])
+  
+  // patch onbeforeunload
+  scoreRef.current = score
+  if (typeof window !== "undefined") {
+      window.addEventListener("beforeunload", (event) => {
+          locationId != 0 && patchLocationScore(locationIdRef.current);
+    });
+  }
 
   return (
     <main id="root" className="flex flex-col items-center justify-between w-screen h-screen pt-16 pb-24">
@@ -19,6 +72,7 @@ export default function Home() {
       <CharacterButtonWithScore
         id={imageId}
         locationId={locationId}
+        _setScore={setScore}
       />
       <CharacterBar setImageId={setImageId}/>
       <RankingBar locationId={locationId}/>
--- rokaf.click/src/components/organisms/CharacterButtonWithScore/index.tsx
+++ rokaf.click/src/components/organisms/CharacterButtonWithScore/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Dispatch, SetStateAction, useEffect, useState, useRef, useLayoutEffect } from 'react'
+import { Dispatch, SetStateAction, useEffect, useState, useRef, useLayoutEffect, RefObject, ForwardedRef } from 'react'
 import { CharacterButton } from "@/src/components/molecules/CharacterButton";
 import { Score } from "@/src/components/atoms/Score";
 import axios from 'axios';
@@ -8,92 +8,44 @@ import axios from 'axios';
 interface CharacterButtonWithScoreProps {
     id: number;
     locationId : number
-    size?: number;
+    _setScore: Dispatch<SetStateAction<number>>
+    // scoreRef: ForwardedRef<number>
 }
 
-export const CharacterButtonWithScore = ({id, locationId}: CharacterButtonWithScoreProps) => {
+export const CharacterButtonWithScore = ({id, locationId, _setScore}: CharacterButtonWithScoreProps) => {
     const [score, setScore] = useState(0)
-    const scoreRef = useRef(0)
-    const [locationScore, setLocationScore] = useState(-1000)
-    const locationScoreRef = useRef(-1000)
+    // const scoreRef = useRef(0)
     const [size, setSize] = useState(0)
-    const locationIdRef = useRef(0)
+    // const locationIdRef = useRef(0)
     
     // responsive image size
     const resizeImage = () => {
         if (id == 45) { setSize(window.innerHeight / 4) }
         else { setSize(window.innerHeight / 3) }
     }
-    
     useEffect(() => {
         if (typeof window !== "undefined") {
             resizeImage()
         }
     }, [id])
-    
     if (typeof window !== "undefined") {
         window.addEventListener("resize", (event) => {
             resizeImage()
         });
     }
 
-    // get location score
-    const getLocationScore = async () => {
-        let score: number = -999
-        try {
-            const response = await axios.get(
-                process.env.NEXT_PUBLIC_API_BASE_PATH + `locations/${locationId}`,
-                )
-                score = response.data.score
-        } catch (e) {
-            console.log(e)
-        } finally {
-            setLocationScore(score)
-        }
-    }
-        
-    // as location id change, call getLocationScore
     useEffect(() => {
-        locationId != 0 && getLocationScore()
-        setScore(0)
+      setScore(0)
     }, [locationId])
-
-    // patch location score
-    locationIdRef.current = locationId
-    const patchLocationScore = async () => {
-        console.log(`locationId: ${locationIdRef.current} locationScore: ${locationScoreRef.current}, score: ${scoreRef.current}`)
-        try {
-            axios.patch(
-                process.env.NEXT_PUBLIC_API_BASE_PATH + `locations/${locationIdRef.current}`,
-                {"score": locationScoreRef.current + scoreRef.current}
-            )
-        } catch (e) {
-            console.log(e)
-        }
-    }
-
-    // as location id change, call patchLocationScore
-    useLayoutEffect(() => {
-        console.log(locationId)
-        // locationId != 0 && patchLocationScore()
-        // setScore(0)
-    }, [locationId])
-    
-    // patch onbeforeunload
-    scoreRef.current = score
-    locationScoreRef.current = locationScore
-    if (typeof window !== "undefined") {
-        window.addEventListener("beforeunload", (event) => {
-            locationId != 0 && patchLocationScore();
-        });
-    }
     
     const characterButtonClick = async () => {
         if (locationId == 0) {
             window.alert("부대를 선택해주세요!")
         } else {
             setScore(score + 1)
-            console.log(`locationScore: ${locationScore}, score: ${score}`)
+            _setScore(score + 1)
+            // scoreRef.current = score + 1
+            // console.log(`locationScore: ${locationScore}, score: ${score}`)
         }
     }
 
