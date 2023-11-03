df858a801ce41a174573cdb6a546cfcd2cd44d2f
Daniel Oh <ohdoyoel@gmail.com>
2023-11-03 14:40:31 +0000

    [design] design success

--- rokaf.click/src/components/molecules/RankingList/index.tsx
+++ rokaf.click/src/components/molecules/RankingList/index.tsx
@@ -34,7 +34,15 @@ export const RankingList = ({rank, setRank, locationId, sortedLocations}: Rankin
 
     return (
         <div className="flex flex-col items-start">
-            <p className="text-xl text-center p-2">🏅 리더보드 {locationId} {rank}</p>
+            <div className="w-full grid grid-cols-3 gap-1 items-center">
+                <p className="text-xl text-left">🏅 리더보드</p>
+                {rank == 0
+                ? <p className="text-sm">부대를 선택해주세요</p>
+                : <div className="col-span-2">
+                        <RankingItem rank={rank} id={locationId} logoSrc={sortedLocations[rank - 1].logoSrc}
+                                    name={sortedLocations[rank - 1].name} score={sortedLocations[rank - 1].score}/>
+                </div>}
+            </div>
             <ul id="locationList" className="h-full w-full p-1 overflow-x-hidden overflow-y-auto text-sm text-gray-700">
                 {list}
             </ul>
