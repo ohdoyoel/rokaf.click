0a66477e77d844def19b46c10cab73b97aed2459
ohdoyoel <ohdoyoel@naver.com>
2025-10-05 18:37:09 +0900

    Refactor Header component for improved readability and formatting

--- rokaf.click/src/components/atoms/Header/index.tsx
+++ rokaf.click/src/components/atoms/Header/index.tsx
@@ -1,11 +1,13 @@
 export const Header = () => {
-    return (
-        <div className='relative flex flex-col items-center scale-90'>
-        <p className='font-thin text-xs absolute left-0 -top-5'>공군 병사가 사지방에서 만든</p>
-        <p className='font-black text-6xl'>
-            rokaf.click
-        </p>
-        <p className='font-medium text-base absolute right-0 -bottom-7'>공군 부대 간 클릭 수로 경쟁합니다.</p>
-        </div>
-    )
-}
+  return (
+    <div className="relative flex flex-col items-center scale-90">
+      <p className="font-thin text-xs absolute left-0 -top-5">
+        공군 병사가 사지방에서 만든
+      </p>
+      <p className="font-black text-6xl">rokaf.click</p>
+      <p className="font-medium text-base absolute right-0 -bottom-7">
+        공군 부대 간 클릭 수로 경쟁합니다.
+      </p>
+    </div>
+  );
+};
